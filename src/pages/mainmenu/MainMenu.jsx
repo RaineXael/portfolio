beb0543d916c3cd3bc67ee6975f8db64828391a3
Brandon P. Olynyk <brandontab2004@gmail.com>
2024-04-11 14:41:31 -0400

    Add tailwind.css

--- src/pages/mainmenu/MainMenu.jsx
+++ src/pages/mainmenu/MainMenu.jsx
@@ -1,7 +1,7 @@
 export function MainMenu(){
     return(
         <div>
-            <h1>Brandon's Portfolio Site</h1>
+            <h1 className="text-3xl font-bold underline">Brandon's Portfolio Site</h1>
         </div>
     );
 }
